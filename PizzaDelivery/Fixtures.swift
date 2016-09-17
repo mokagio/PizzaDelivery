d46ad174e464c53b696da2afec9500325327725f
Giovanni Lodi <giovanni.lodi42@gmail.com>
2016-09-18 09:43:02 +1000

    Add fixture function with array of pizzas

---
+++ PizzaDelivery/Fixtures.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+func fixtureMenu() -> [Pizza] {
+  return [
+    Pizza(price: 8, name: "Margherita"),
+    Pizza(price: 10, name: "Capricciosa"),
+    Pizza(price: 12, name: "Quattro Stagioni"),
+    Pizza(price: 12.5, name: "Montanara"),
+    Pizza(price: 11, name: "Piccante"),
+    Pizza(price: 10, name: "Quattro Formaggi"),
+    Pizza(price: 13.5, name: "Romana"),
+    Pizza(price: 15, name: "Mari e Monti"),
+    Pizza(price: 12.5, name: "Grana e Rucola"),
+    Pizza(price: 11, name: "Boscaiola"),
+    Pizza(price: 10.5, name: "Rustica"),
+  ]
+}
