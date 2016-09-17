45f984af3c5eac974587247f8ebb97c24f14a62c
Giovanni Lodi <giovanni.lodi42@gmail.com>
2016-09-18 09:52:26 +1000

    Implement table view data source

--- PizzaDelivery/PizzaListViewController.swift
+++ PizzaDelivery/PizzaListViewController.swift
@@ -4,7 +4,41 @@ class PizzaListViewController: UIViewController {
 
   @IBOutlet var tableView: UITableView!
 
+  let pizzaCellIdentifier = "pizza"
+
+  let data = fixtureMenu()
+
   override func viewDidLoad() {
     super.viewDidLoad()
+
+    title = "🍕"
+
+    tableView.dataSource = self
+
+    tableView.register(UITableViewCell.self, forCellReuseIdentifier: pizzaCellIdentifier)
+  }
+}
+
+extension PizzaListViewController: UITableViewDataSource {
+
+  func numberOfSections(in tableView: UITableView) -> Int {
+    return 1
+  }
+
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return data.count
+  }
+
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let pizza = data[indexPath.row]
+    let cell = tableView.dequeueReusableCell(withIdentifier: pizzaCellIdentifier, for: indexPath)
+
+    configure(cell, with: pizza)
+
+    return cell
+  }
+
+  private func configure(_ cell: UITableViewCell, with pizza: Pizza) {
+    cell.textLabel?.text = "\(pizza.name) ($\(pizza.price))"
   }
 }
