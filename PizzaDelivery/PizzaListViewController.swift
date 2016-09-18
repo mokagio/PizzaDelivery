88ffe28db24730cc7601a14a2062f4a8deab79ea
Giovanni Lodi <giovanni.lodi42@gmail.com>
2016-09-19 07:27:25 +1000

    Tidy up the code

--- PizzaDelivery/PizzaListViewController.swift
+++ PizzaDelivery/PizzaListViewController.swift
@@ -22,18 +22,12 @@ class PizzaListViewController: UIViewController {
 
     title = "🍕"
 
+    configureTableView()
+    errorRetryButton.addTarget(self, action: #selector(loadPizzasList), for: .touchUpInside)
+
     spinner.hidesWhenStopped = true
-    tableView.dataSource = self
-
-    tableView.register(UITableViewCell.self, forCellReuseIdentifier: pizzaCellIdentifier)
-    tableView.register(UITableViewCell.self, forCellReuseIdentifier: adCellIdentifier)
-
-    tableView.tableFooterView = UIView()
-
     tableView.isHidden = true
     errorView.isHidden = true
-
-    errorRetryButton.addTarget(self, action: #selector(loadPizzasList), for: .touchUpInside)
   }
 
   override func viewDidAppear(_ animated: Bool) {
@@ -43,7 +37,7 @@ class PizzaListViewController: UIViewController {
     loadPizzasList()
   }
 
-  func loadPizzasList() {
+  internal func loadPizzasList() {
     tableView.isHidden = true
     errorView.isHidden = true
     spinner.startAnimating() // starting animation shows as well
@@ -71,6 +65,15 @@ class PizzaListViewController: UIViewController {
       }
     }
   }
+
+  private func configureTableView() {
+    tableView.dataSource = self
+
+    tableView.register(UITableViewCell.self, forCellReuseIdentifier: pizzaCellIdentifier)
+    tableView.register(UITableViewCell.self, forCellReuseIdentifier: adCellIdentifier)
+
+    tableView.tableFooterView = UIView()
+  }
 }
 
 extension PizzaListViewController: UITableViewDataSource {
@@ -95,24 +98,29 @@ extension PizzaListViewController: UITableViewDataSource {
     // Every x pizzas display an ad
     let interval = 4
     let displayedAds = indexPath.row / interval
+    let shouldDisplayPizza = indexPath.row < (interval - 1) || indexPath.row % interval != 0
 
-    if indexPath.row < (interval - 1) || indexPath.row % interval != 0 {
+    if shouldDisplayPizza {
       let cell = tableView.dequeueReusableCell(withIdentifier: pizzaCellIdentifier, for: indexPath)
       let pizza = data[indexPath.row - displayedAds]
-      configure(cell, with: pizza)
+      configure(cell: cell, with: pizza)
       return cell
     } else {
       let cell = tableView.dequeueReusableCell(withIdentifier: adCellIdentifier, for: indexPath)
       let ad = ads[displayedAds - 1]
-      cell.textLabel?.text = ad.message
-      cell.textLabel?.textAlignment = .center
-      cell.textLabel?.textColor = UIColor.white
-      cell.backgroundColor = ad.color
+      configure(cell: cell, with: ad)
       return cell
     }
   }
 
-  private func configure(_ cell: UITableViewCell, with pizza: Pizza) {
+  private func configure(cell: UITableViewCell, with pizza: Pizza) {
     cell.textLabel?.text = "\(pizza.name) ($\(pizza.price))"
+  }
+
+  private func configure(cell: UITableViewCell, with ad: Ad) {
+    cell.textLabel?.text = ad.message
+    cell.textLabel?.textAlignment = .center
+    cell.textLabel?.textColor = UIColor.white
+    cell.backgroundColor = ad.color
   }
 }
