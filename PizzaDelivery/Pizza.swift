651e9dd739cb457124cb01bf4f34333943503e7e
Giovanni Lodi <giovanni.lodi42@gmail.com>
2016-09-18 11:43:12 +1000

    Implement JSON decoding for Pizza

--- PizzaDelivery/Pizza.swift
+++ PizzaDelivery/Pizza.swift
@@ -2,3 +2,16 @@ struct Pizza {
   let price: Double
   let name: String
 }
+
+extension Pizza {
+  init?(jsonObject: [String: Any]) {
+    guard
+      let name = jsonObject["name"] as? String,
+      let price = jsonObject["price"] as? Double
+      else {
+        return nil
+    }
+
+    self.init(price: price, name: name)
+  }
+}
