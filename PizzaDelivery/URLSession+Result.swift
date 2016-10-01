335901157bef1bc3c4571e06774d97acce83e502
Giovanni Lodi <giovanni.lodi42@gmail.com>
2016-10-01 14:10:28 +1000

    Add URLSession extension using Result

---
+++ PizzaDelivery/URLSession+Result.swift
@@ -0,0 +1,54 @@
+import Foundation
+
+extension URLSession {
+
+  func yow_dataTask(with url: URL, completion: @escaping (Result<(Data, URLResponse), NetworkingError>) -> ()) -> URLSessionDataTask {
+    return dataTask(with: url) { data, response, error in
+      switch (data, response, error) {
+      case (.some(let data), .some(let response), .none):
+        completion(Result(value: (data, response)))
+      case (.none, .none, .some(let error)):
+        completion(Result(error: .wrapped(error)))
+      case (_, _, _):
+        completion(Result(error: .inconsistentResponse))
+      }
+    }
+  }
+
+  func yow_dataTask(with url: URL, completion: @escaping (Result<(Data, HTTPStatusCode), NetworkingError>) -> ()) -> URLSessionDataTask {
+    return yow_dataTask(with: url) { (result: Result<(Data, URLResponse), NetworkingError>) -> () in
+      completion(
+        result.flatMap { (data, response) -> Result<(Data, HTTPStatusCode), NetworkingError> in
+          guard let httpResponse = response as? HTTPURLResponse else {
+            return Result(error: .notHTTPResponse)
+          }
+
+          return Result(value: (data, httpResponse.statusCode))
+        }
+      )
+    }
+  }
+
+  func yow_dataTask(with url: URL, completion: @escaping (Result<Data, NetworkingError>) -> ()) -> URLSessionDataTask {
+    return yow_dataTask(with: url) { (result: Result<(Data, HTTPStatusCode), NetworkingError>) -> () in
+      completion(
+        result.flatMap { (data, statusCode) -> Result<Data, NetworkingError> in
+          if statusCode >= 400 {
+            return Result(error: .httpError(statusCode))
+          } else {
+            return Result(value: data)
+          }
+        }
+      )
+    }
+  }
+}
+
+typealias HTTPStatusCode = Int
+
+enum NetworkingError: Error {
+  case notHTTPResponse
+  case httpError(HTTPStatusCode)
+  case wrapped(Error)
+  case inconsistentResponse
+}
