22b7c1cb8a5172fd76dfd6106728e86928db58df
Giovanni Lodi <giovanni.lodi42@gmail.com>
2016-10-01 12:24:56 +1000

    Define JSONObject and JSONArray aliases

---
+++ PizzaDelivery/JSON.swift
@@ -0,0 +1,7 @@
+// The full JSON spec can be found at http://www.json.org/
+//
+// Using `Any` is not 100% percent correct to represent a JSON object, but it's
+// acceptable for the practical purposes of this app.
+typealias JSONObject = [String: Any]
+
+typealias JSONArray = [JSONObject]
