92c4cb579c24115ecad3b3833ee069d82e2b2a80
Giovanni Lodi <giovanni.lodi42@gmail.com>
2016-10-05 06:35:08 +1100

    Replace NSError with Error enum in PizzaService

--- PizzaDelivery/PizzaService.swift
+++ PizzaDelivery/PizzaService.swift
@@ -10,52 +10,31 @@ class PizzaService {
     self.baseURL = baseURL
   }
 
-  func loadPizzas(completion: @escaping ([Pizza]?, NSError?) -> ()) {
+  func loadPizzas(completion: @escaping ([Pizza]?, Error?) -> ()) {
     session.dataTask(with: baseURL.appendingPathComponent("pizzas")) { data, response, error in
       switch (data, response, error) {
       case (.some(let data), .some, _):
         do {
           guard let jsonObject = try JSONSerialization.jsonObject(with: data, options: []) as? JSONObject else {
-            completion(.none, PizzaService.MissingContentError)
+            completion(.none, PizzaServiceError.missingContent)
             return
           }
 
           let pizzaListResponse = try JSONParser.pizzaList(fromJSON: jsonObject)
           completion(pizzaListResponse.list, .none)
-        } catch let e as NSError {
-          completion(.none, e)
         } catch {
-          completion(.none, PizzaService.errorWrapping(error))
+          completion(.none, error)
         }
-      case (_, _, .some(let error as NSError)):
-        completion(.none, error)
       case (_, _, .some(let error)):
-        completion(.none, PizzaService.errorWrapping(error))
+        completion(.none, error)
       case _:
-        completion(.none, PizzaService.InconsistenResponseError)
+        completion(.none, PizzaServiceError.inconsistentResponse)
       }
     }.resume()
   }
+}
 
-  private static let errorDomain = "pizzaservice"
-
-  static let InconsistenResponseError = NSError(
-    domain: errorDomain,
-    code: 1,
-    userInfo: [NSLocalizedDescriptionKey: "Inconsistent network response"]
-  )
-
-  static let MissingContentError = NSError(
-    domain: errorDomain,
-    code: 2,
-    userInfo: [NSLocalizedDescriptionKey: "The network response is missing the expected data"]
-  )
-
-  static func errorWrapping(_ error: Error) -> NSError {
-    return NSError(
-      domain: errorDomain,
-      code: 3,
-      userInfo: [NSLocalizedDescriptionKey: "\(error)"]
-    )
-  }
+enum PizzaServiceError: Error {
+  case inconsistentResponse
+  case missingContent
 }
